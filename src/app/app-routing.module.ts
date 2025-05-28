ba77dfa32dd0bc8e1c4f01b04ff49bc6af78ef86
Amalesh Arivanan <amalesh.unofficial@gmail.com>
2025-05-28 13:14:44 -0700

    Tenth Commit: Final routes and styling updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductlistComponent } from './components/productlist/productlist.component';
-
+import { ProductdetailsComponent } from './components/productdetails/productdetails.component';
 const routes: Routes = [
   { path: '', redirectTo: 'products', pathMatch: 'full' },
   { path: 'products', component: ProductlistComponent },
+  { path: 'products/:id', component: ProductdetailsComponent }
 ];
 
 @NgModule({
