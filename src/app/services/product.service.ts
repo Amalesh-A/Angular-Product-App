cf0de0ec162fe95f40625dcac3bd44d50bcd6616
Amalesh Arivanan <amalesh.unofficial@gmail.com>
2025-05-28 13:05:24 -0700

    Nineth Commit: Updated CSS

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -6,11 +6,11 @@ import { Observable, of } from 'rxjs';
 })
 export class ProductService {
   private products = [
-    {id: 1, name: 'Product 1', price: 100, description: 'This is the first product'},
-    {id: 2, name: 'Product 2', price: 200, description: 'This is the second product'},
-    {id: 3, name: 'Product 3', price: 300, description: 'This is the third product'},
-    {id: 4, name: 'Product 4', price: 400, description: 'This is the fourth product'},
-    {id: 5, name: 'Product 5', price: 500, description: 'This is the fifth product'},
+    {id: 1, name: 'Product 1', price: 100, description: 'This is the first product', link: 'https://www.link1.com'},
+    {id: 2, name: 'Product 2', price: 200, description: 'This is the second product', link: 'https://www.link2.com'},
+    {id: 3, name: 'Product 3', price: 300, description: 'This is the third product', link: 'https://www.link3.com'},
+    {id: 4, name: 'Product 4', price: 400, description: 'This is the fourth product', link: 'https://www.link4.com'},
+    {id: 5, name: 'Product 5', price: 500, description: 'This is the fifth product', link: 'https://www.link5.com'},
   ];
   getProducts(): Observable<any[]> {
     return of(this.products);
